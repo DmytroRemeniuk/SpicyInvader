6477230d0ed0ab2ca3e581915c65abe185e30d4d
Dmytro Remeniuk <pb07iho@eduvaud.ch>
2024-01-18 16:11:21 +0100

    Menu(?)

--- SpicyInvader/Program.cs
+++ SpicyInvader/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    ETML
+    Autor         : Dmytro Remeniuk
+    Date          : 18.01.2024
+    Description   : the game Space Invader (Spicy Invader) on the console
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +16,63 @@ namespace SpicyInvader
     {
         static void Main(string[] args)
         {
+            #region Variables
+            string menuChoice = "";//to get the user's input
+            #endregion
+
+            do
+            {
+                //write the menu's content
+                Console.WriteLine("    Menu\n" +
+                                  "[1] Jouer\n" +
+                                  "[2] Options\n" +
+                                  "[3] Voir le highscore\n" +
+                                  "[4] A propos\n" +
+                                  "[Q] Quitter");
+                menuChoice = Console.ReadLine().ToUpper();//recover the choice 
+                //make sure the answer is appropriate to the menu content
+                while(menuChoice != "1" && menuChoice != "2" && menuChoice != "3" && menuChoice != "4" && menuChoice != "Q")
+                {
+                    Console.WriteLine("Veuillez choisir une des options du menu");
+                    menuChoice = Console.ReadLine().ToUpper();
+                }
+
+                //verify the choice and pass to the chosen stage
+                switch(menuChoice)
+                {
+                    case "1":
+                        Console.WriteLine("Jeu");
+                        ContinueQuit();
+                        break;
+                    case "2":
+                        Console.WriteLine("Options");
+                        ContinueQuit();
+                        break ;
+                    case "3":
+                        Console.WriteLine("Highscore");
+                        ContinueQuit();
+                        break;
+                    case "4":
+                        Console.WriteLine("A propos");
+                        ContinueQuit();
+                        break;
+                }
+            }while (menuChoice != "Q");//quit if "Q"
+
+            //method that asks every time if the user wants to continue or quit
+            void ContinueQuit()
+            {
+                Console.WriteLine("Continuer [C] ou quitter [q]?");
+                menuChoice = Console.ReadLine().ToUpper();
+                //make sure that only "C" or "Q" will be entered
+                while (menuChoice != "C" && menuChoice != "Q")
+                {
+                    Console.WriteLine("Veuillez choisir une des options");
+                    menuChoice = Console.ReadLine().ToUpper();
+                }
+            }
         }
+
+        
     }
 }

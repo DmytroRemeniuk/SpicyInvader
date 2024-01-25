2e2aeeda0d76ae7420f5cd638cbbcba193c9492c
Dmytro Remeniuk <pb07iho@eduvaud.ch>
2024-01-25 12:19:10 +0100

    moving

--- SpicyInvader/Program.cs
+++ SpicyInvader/Program.cs
@@ -17,9 +17,31 @@ namespace SpicyInvader
         static void Main(string[] args)
         {
             #region Variables
-            string menuChoice = "";//to get the user's input
+            string menuChoice = "";//to recover the user's input
+            int x;//to recover the spaceship's position
+            int oldX = 0;//stock of x
             #endregion
 
+            #region Constants
+            //for the size of window
+            const int WINDOW_WIDTH = 125;
+            const int WINDOW_HEIGHT = 55;
+            //start position
+            const int START_X = 20;
+            const int START_Y = 50;
+            //limitation
+            const int MAX_Y = 120;
+            const int MIN_Y = 0;
+            //spaceship
+            const string SPACESHIP = "«=ˆ=»";
+            #endregion
+
+            //set the size of the console
+            Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+            //hide the cursor
+            Console.CursorVisible = false;
+
+            //menu and a loop to continue the game
             do
             {
                 //write the menu's content
@@ -41,7 +63,7 @@ namespace SpicyInvader
                 switch(menuChoice)
                 {
                     case "1":
-                        Console.WriteLine("Jeu");
+                        Game();
                         ContinueQuit();
                         break;
                     case "2":
@@ -71,8 +93,44 @@ namespace SpicyInvader
                     menuChoice = Console.ReadLine().ToUpper();
                 }
             }
-        }
 
-        
+            //game method
+            void Game()
+            {
+                Console.Clear();
+                //display in a specific location
+                Console.SetCursorPosition(START_X, START_Y);
+                x = START_X-1;
+                Console.WriteLine(SPACESHIP);
+
+                do
+                {
+                    //moving
+                    switch(Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.RightArrow:
+                            if(x < MAX_Y)
+                            {
+                                oldX = x;
+                                x++;
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if(x > MIN_Y)
+                            {
+                                oldX = x;
+                                x--;
+                            }
+                            break ;
+                    }
+                    Console.SetCursorPosition(oldX, START_Y);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(oldX + SPACESHIP.Length-1, START_Y);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(x, START_Y);
+                    Console.Write(SPACESHIP);
+                } while(true);
+            }
+        }
     }
 }
